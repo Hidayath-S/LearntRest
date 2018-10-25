edde9c5b8decfb91484416bd13077b83207f95b8
Hidayath <Hidayath@DESKTOP-K1VIJ2U>
2018-10-25 22:02:13 +0530

    Updated pom.xml and added testng.xml

--- LearnRest/src/main/java/com/demo/Test1_BasicFeatures.java
+++ LearnRest/src/main/java/com/demo/Test1_BasicFeatures.java
@@ -7,7 +7,7 @@ import static org.hamcrest.Matchers.*;
 public class Test1_BasicFeatures {
 	
 	// check the status code
-	//@Test
+	@Test
 	public void testStatusCode(){
 		given().get("https://jsonplaceholder.typicode.com/posts/3").
 		then().statusCode(200);
@@ -19,7 +19,7 @@ public class Test1_BasicFeatures {
 		then().
 		body("RestResponse.result.name",equalTo("United States of America")).log().all();
 	}
-	//@Test
+	@Test
 	public void testHasItemFunction(){
 		given().get("http://services.groupkt.com/country/get/all").
 		then().
